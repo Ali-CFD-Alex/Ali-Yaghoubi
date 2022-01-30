cce670e9aaf856f206ffa4d0427c3ed6106ad18c
Ali <yaali4422519@gmail.com>
2022-01-30 22:14:34 +0330

    update
    Signed-off-by: Ali <yaali4422519@gmail.com>

--- Character Count/Reciever.py
+++ Character Count/Reciever.py
@@ -1,15 +1,8 @@
 # https://github.com/Ali-CFD-Alex
 data=input("Matn ra vared konid : ")
-# i baraye ijad halghe
 i=0
 inv=0
 while(i<len(data)):
-    # Peyda kardan addad dar beyn matn
     if(data[i].isnumeric()==True):
-        # namayesh matn az arraye khane i+1 ta mehgdar khane i+1
-        # agar avalin adad 6 bashad baraye "Hello" dar arraye darim [6,H,e,l,l,o]
-        # pas ma bayad az mogheyiat (index) avalin khane +1 ke mishavad haman 1 yani "H"
-        # ta meghdar avalin khane yani 6 balaveye 1 (7 chon ke ta 7 chap konad vali khod khane 7 ro na agar 6 khali bod dar mesal o ra chap nemikard)
         print(data[i+1:int(data[i])+i])
-        # rad shodan az in kalame va raftan be kalame baad
         i+=int(data[i])
--- Character Count/Sender.py
+++ Character Count/Sender.py
@@ -1,18 +1,12 @@
 # https://github.com/Ali-CFD-Alex
-# daryaft tedad frame ha baraye ijad arraye
 n=int(input("Tedad Frame ha (int) : "))
 l=[]
 l2=[]
-# Halghe baraye daryaft mohtavaye har frame
 for i in range(0,n):
   x=input("{} {} {} ".format("Matn Frame", i+1, ":"))
-  # Ezafe kardan matn be arraye l
   l.append(x)
-  # Ezafe kardan tol payam +1 braye ghaleb bandidar samt girande
   l2.append(len(x)+1)
-
 sender=''
-# halghe baraye tarkib kardan tol payam va khod payam
 for i in range(0,n):
     sender+= str(l2[i])+l[i]
 print("{} {}".format("Payam :",sender))
